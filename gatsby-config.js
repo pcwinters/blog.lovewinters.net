29ef3dea0641a628b36e1bab545bceaa7734506a
Patrick Winters <patrick.winters@citrix.com>
2019-02-23 15:39:44 -0500

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,13 +43,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Love-Winters Family Blog`,
+        short_name: `lovewinters`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
